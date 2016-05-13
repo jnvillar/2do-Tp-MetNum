dbfe2f323224acb8c74e3313e7eb0f66ba89a9b8
lew axel ariel <alew@ws13.labo7>
2016-05-13 19:43:40 -0300

    experimento1

--- Experimentacion/ExperimentoKnn/knn.cpp
+++ Experimentacion/ExperimentoKnn/knn.cpp
@@ -9,36 +9,43 @@ int main(int argc, char* argv[]){
 		exit(1);
 	}
 
-	int cantTrain = atoi(argv[2]);
-	int cantTest = atoi(argv[3]);
-	Matriz m = parser(argv[1],cantTrain);
-	Matriz imagenesTest = parserImgTest(argv[1],cantTrain+1,cantTrain+1+cantTest);
+	FILE* out = fopen("result.txt","w");
 
-	int cantVecinos = atoi(argv[4]);
+	for(int j = 0; j<35; j++){
 
-	float tiempo = 0;
-	float hitRate;
-	for(int h = 0; h<50; h++){
-		clock_t t = clock();
+		int cantTrain = atoi(argv[2]);
+		int cantTest = atoi(argv[3]);
+		Matriz m = parser(argv[1],cantTrain);
+		Matriz imagenesTest = parserImgTest(argv[1],cantTrain+1,cantTrain+1+cantTest);
+		int cantVecinos = atoi(argv[4])+j*10;
 
 
-		int aciertos = 0;
-		for(int i = 0; i<imagenesTest.Filas(); i++){
-			vector<float> fila = imagenesTest.obtenerFila(i);
-			int res = m.caenene(cantVecinos, fila);
-			if (res == imagenesTest.digitoRepresentado(i)){
-				aciertos++;
-			} else {
+		float tiempo = 0;
+		float hitRate;
+		for(int h = 0; h<50; h++){
+			clock_t t = clock();
+
+			int aciertos = 0;
+			for(int i = 0; i<imagenesTest.Filas(); i++){
+				vector<float> fila = imagenesTest.obtenerFila(i);
+				int res = m.caenene(cantVecinos, fila);
+				if (res == imagenesTest.digitoRepresentado(i)){
+					aciertos++;
+				} else {
+				}
 			}
+			hitRate = (float )aciertos/(float )imagenesTest.Filas();
+			t = clock() - t;
+			tiempo += (((float)t)/CLOCKS_PER_SEC);
 		}
-		hitRate = (float )aciertos/(float )imagenesTest.Filas();
-		t = clock() - t;
-		tiempo += (((float)t)/CLOCKS_PER_SEC);
-	}
 		tiempo = tiempo/50;
+		fprintf(out, "%f\n", tiempo);
 		cout << "tiempo: " << tiempo << endl;
 		cout << "hitRate: " << hitRate << endl;
 
+
+	}
+	fclose(out);
 	return 0;
   
 }
