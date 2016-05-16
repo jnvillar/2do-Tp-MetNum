6d0bce039b39eccf0ba3138625acf3a1146c6a7b
juany360 <juaninv@outlook.com>
2016-05-16 13:24:36 -0300

    exp 3

--- Experimentacion/Experimento PCA y PLS-DA/pca-pls:exp3.cpp
+++ Experimentacion/Experimento PCA y PLS-DA/pca-pls:exp3.cpp
@@ -2,15 +2,29 @@
 #include <stdio.h>
 #include <string.h>
 
-vector<vector<float> > crearvector(){
-	vector<float> aux(11,0);
-	vector<float> aux2(1,0);
+vector<vector<float> > crearvector(int k,int j){
+	vector<float> aux(k,0);
 	vector<vector<float> > res;
-	res.push_back(aux);
-	res.push_back(aux);
-	res.push_back(aux);
-	res.push_back(aux2);
+	for (int i = 0; i < j; ++i){
+		res.push_back(aux);
+	}		
 	return res;
+}
+
+void sumMat(vector<vector<float> > &m1,vector<vector<float> > &m2){
+	for (int i = 0; i < m1.size() ; ++i){
+		for (int j = 0; j < m1[i].size(); ++j){
+			m1[i][j] = m1[i][j]+m2[i][j];
+		}			
+	}	
+}
+
+void promedio(vector<vector<float> > &m, float k){
+	for (int i = 0; i < m.size(); ++i){
+		for (int j = 0; j < m[i].size(); ++j){
+			m[i][j] = m[i][j]/k;
+		}
+	}
 }
 
 int main(int argc, char* argv[]){
@@ -34,10 +48,17 @@ int main(int argc, char* argv[]){
 	Matriz imagenesPls = parser(argv[1],cantIm); 	// ENTRADA, CANTIDAD DE IMAGENES
 	Matriz imagenesKnn = parser(argv[1],cantIm); 	// ENTRADA, CANTIDAD DE IMAGENES
 
+	vector<float> aux(1,0);
+	vector<vector<float> > metricasPromPca = crearvector(11,3);
+	metricasPromPca.push_back(aux);
+	vector<vector<float> > metricasPromPls = crearvector(11,3);
+	metricasPromPls.push_back(aux);
+	vector<vector<float> > metricasPromKnn = crearvector(11,3);
+	metricasPromKnn.push_back(aux);
 
-	vector<vector<float> > metricasPromPca = crearvector();
-	vector<vector<float> > metricasPromPls = crearvector();
-	vector<vector<float> > metricasPromKnn = crearvector();
+	vector<vector<float> > confPca = crearvector(10,10);
+	vector<vector<float> > confPls = crearvector(10,10);
+	vector<vector<float> > confKnn = crearvector(10,10);
 
 	for(int i = 0; i<k; i++){
 		Matriz testpca = imagenesPca.subMatriz(i*tamConj,(i+1)*tamConj);
@@ -47,87 +68,89 @@ int main(int argc, char* argv[]){
 		Matriz trainpls = imagenesPls.subMatriz(0,i*tamConj,(i+1)*tamConj,cantIm);
 		Matriz trainknn = imagenesKnn.subMatriz(0,i*tamConj,(i+1)*tamConj,cantIm);
 
-		vector<vector<float> > metricasPca= usarPca(trainpca,testpca,cantAutov,cantIterMetPot,cantVecinos);
-		vector<vector<float> > metricasPls= usarPls(trainpls,testplc,cantAutov,cantIterMetPot,cantVecinos);
-		vector<vector<float> > metricasKnn= usarKnn(trainknn,testknn,cantVecinos);	
+		pair<vector<vector<float> >,vector<vector<float> > > metricasPca= usarPca2(trainpca,testpca,cantAutov,cantIterMetPot,cantVecinos);
+		pair<vector<vector<float> >,vector<vector<float> > > metricasPls= usarPls2(trainpls,testplc,cantAutov,cantIterMetPot,cantVecinos);
+		pair<vector<vector<float> >,vector<vector<float> > > metricasKnn= usarKnn2(trainknn,testknn,cantVecinos);	
+		
+		sumMat(metricasPromPca,metricasPca.first);
+		sumMat(metricasPromPls,metricasPls.first);
+		sumMat(metricasPromKnn,metricasKnn.first);
 
-		for (int i = 0; i < 11; ++i){			
-			metricasPromPca[0][i] += metricasPca[0][i];
-			metricasPromPls[0][i] += metricasPca[0][i];
-			metricasPromKnn[0][i] += metricasPca[0][i];
-
-			metricasPromPca[1][i] += metricasPca[1][i];
-			metricasPromPls[1][i] += metricasPls[1][i];
-			metricasPromKnn[1][i] += metricasPca[1][i];
-
-			metricasPromPca[2][i] += metricasPca[2][i];
-			metricasPromPls[2][i] += metricasPls[2][i];
-			metricasPromKnn[2][i] += metricasKnn[2][i];
-		}
-			metricasPromPca[3][0] += metricasPca[3][0];
-			metricasPromPls[3][0] += metricasPls[3][0];
-			metricasPromKnn[3][0] += metricasKnn[3][0];	
+		sumMat(confPca,metricasPca.second);
+		sumMat(confPls,metricasPls.second);
+		sumMat(confKnn,metricasKnn.second);
+		
 	}
 
 	//SACO PROMEDIO
 
-	for (int i = 0; i < 11; ++i){
-		metricasPromPca[0][i] = metricasPromPca[0][i]/k;
-		metricasPromPls[0][i] = metricasPromPls[0][i]/k;
-		metricasPromKnn[0][i] = metricasPromKnn[0][i]/k;
+	promedio(metricasPromPca,k);
+	promedio(metricasPromPls,k);
+	promedio(metricasPromKnn,k);
+	promedio(confPca,k);
+	promedio(confPls,k);
+	promedio(confKnn,k);
+	
+	// IMPRIMO PRESICION
+		fprintf(out1, "%s\n", "Precision");			
+		fprintf(out2, "%s\n", "Presicion");				
+		fprintf(out3, "%s\n", "Presicion");	
 
-		metricasPromPca[1][i] = metricasPromPca[1][i]/k;
-		metricasPromPls[1][i] = metricasPromPls[1][i]/k;
-		metricasPromKnn[1][i] = metricasPromKnn[1][i]/k;
-
-		metricasPromPca[2][i] = metricasPromPca[2][i]/k;
-		metricasPromPls[2][i] = metricasPromPls[2][i]/k;
-		metricasPromKnn[2][i] = metricasPromKnn[2][i]/k;		
+	for (int i = 0; i < 11; ++i){			
+		fprintf(out1, "Numero: %d ,%f\n", i, metricasPromPca[0][i]);			
+		fprintf(out2, "Numero: %d ,%f\n", i, metricasPromPls[0][i]);				
+		fprintf(out3, "Numero: %d ,%f\n", i, metricasPromKnn[0][i]);			
 	}
 
-		metricasPromPca[3][0] = metricasPromPca[3][0]/k;
-		metricasPromPls[3][0] = metricasPromPls[3][0]/k;
-		metricasPromKnn[3][0] = metricasPromKnn[3][0]/k;	
+	// IMPRIMO RECALL
+		fprintf(out1, "%s\n", "Recall");			
+		fprintf(out2, "%s\n", "Recall");				
+		fprintf(out3, "%s\n", "Recall");	
 
-	// IMPRIMO	
+	for (int i = 0; i < 11; ++i){
+		fprintf(out1, "Numero: %d ,%f\n", i, metricasPromPca[1][i]);			
+		fprintf(out2, "Numero: %d ,%f\n", i, metricasPromPls[1][i]);				
+		fprintf(out3, "Numero: %d ,%f\n", i, metricasPromKnn[1][i]);			
+	}
 
-		for (int i = 0; i < 11; ++i){
-			fprintf(out1, "%s\n", "Precision");			
-			fprintf(out2, "%s\n", "Presicion");				
-			fprintf(out3, "%s\n", "Presicion");	
+	// IMPRIMO F1-SCORE
+		fprintf(out1, "%s\n", "F1-Score");			
+		fprintf(out2, "%s\n", "F1-Score");				
+		fprintf(out3, "%s\n", "F1-Score");	
 
-			fprintf(out1, "Numero: %d ,%f\n", i, metricasPromPca[0][i]);			
-			fprintf(out2, "Numero: %d ,%f\n", i, metricasPromPls[0][i]);				
-			fprintf(out3, "Numero: %d ,%f\n", i, metricasPromKnn[0][i]);			
-		}
+	for (int i = 0; i < 11; ++i){
+		fprintf(out1, "Numero: %d ,%f\n", i, metricasPromPca[2][i]);			
+		fprintf(out2, "Numero: %d ,%f\n", i, metricasPromPls[2][i]);				
+		fprintf(out3, "Numero: %d ,%f\n", i, metricasPromKnn[2][i]);			
+	}
 
-		for (int i = 0; i < 11; ++i){
-			fprintf(out1, "%s\n", "Recall");			
-			fprintf(out2, "%s\n", "Recall");				
-			fprintf(out3, "%s\n", "Recall");	
-
-			fprintf(out1, "Numero: %d ,%f\n", i, metricasPromPca[1][i]);			
-			fprintf(out2, "Numero: %d ,%f\n", i, metricasPromPls[1][i]);				
-			fprintf(out3, "Numero: %d ,%f\n", i, metricasPromKnn[1][i]);			
-		}
-
-		for (int i = 0; i < 11; ++i){
-			fprintf(out1, "%s\n", "F1-Score");			
-			fprintf(out2, "%s\n", "F1-Score");				
-			fprintf(out3, "%s\n", "F1-Score");	
-
-			fprintf(out1, "Numero: %d ,%f\n", i, metricasPromPca[2][i]);			
-			fprintf(out2, "Numero: %d ,%f\n", i, metricasPromPls[2][i]);				
-			fprintf(out3, "Numero: %d ,%f\n", i, metricasPromKnn[2][i]);			
-		}
+	// IMPRIMO HITRATE
 
 	fprintf(out1, "%s\n", "Hit rate");			
 	fprintf(out2, "%s\n", "Hit rate");				
 	fprintf(out3, "%s\n", "Hit rate");	
-
 	fprintf(out1, "%f\n", metricasPromPca[3][0]);			
 	fprintf(out2, "%f\n", metricasPromPls[3][0]);				
 	fprintf(out3, "%f\n", metricasPromKnn[3][0]);
+
+	// IMPRIMO MATCONF
+
+	fprintf(out1, "%s\n", "MatConf");			
+	fprintf(out2, "%s\n", "MatConf");				
+	fprintf(out3, "%s\n", "MatConf");	
+
+	for (int i = 0; i < 10; ++i){
+		for (int j = 0; j < 10; ++j){
+		fprintf(out1, "%f,", confPca[i][j]);			
+		fprintf(out2, "%f,", confPls[i][j]);				
+		fprintf(out3, "%f,", confKnn[i][j]);	
+		}
+		fprintf(out1, "\n");			
+		fprintf(out2, "\n");				
+		fprintf(out3, "\n");	
+	}
+
+
 
 return 0;
 
