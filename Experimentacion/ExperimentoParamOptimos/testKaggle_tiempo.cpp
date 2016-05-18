dc6d58829d62fec35e8212d8f7136dffb5a5a385
Matias Ezequiel Cadaval <mcadaval@ws19.labo7>
2016-05-18 17:20:51 -0300

    mas de test kaggle

--- Experimentacion/ExperimentoParamOptimos/testKaggle_tiempo.cpp
+++ Experimentacion/ExperimentoParamOptimos/testKaggle_tiempo.cpp
@@ -4,72 +4,108 @@
 #include <fstream>
 #include <time.h>       /* clock_t, clock, CLOCKS_PER_SEC */
 
-
 //Parametros optimos Vecinos=5 metoPot=20 K-cross= 5 a 7 aprox cant_autovectores_pls = ?? cant_autovectores_pca = ??   
 int main(int argc, char* argv[]){
-  if (argc != 5 && argc != 7){
+  if (argc != 6 && argc != 8){
     cout << "USO:" << endl;
-    cout << "PSA: " << argv[0] << " pca [INPUT FILE TRAIN] [INPUT FILE TEST] [CANT DE AUTOVECTORES] [CANTIDAD ITERACIONES METODO POTENCIA] [CANTIDAD VECINOS]" << endl;
-    cout << "PLS: " << argv[0] << " pls [INPUT FILE TRAIN] [INPUT FILE TEST] [CANT DE ITERACIONES PLS] [CANTIDAD ITERACIONES METODO POTENCIA] [CANTIDAD VECINOS]" << endl;
-    cout << "KNN: " << argv[0] << " knn [INPUT FILE TRAIN] [INPUT FILE TEST] [CANTIDAD VECINOS]" << endl;
+    cout << "PSA: " << argv[0] << " pca [INPUT FILE TRAIN] [INPUT FILE TEST] [OUTPUT FILE] [CANT DE AUTOVECTORES] [CANTIDAD ITERACIONES METODO POTENCIA] [CANTIDAD VECINOS]" << endl;
+    cout << "PLS: " << argv[0] << " pls [INPUT FILE TRAIN] [INPUT FILE TEST] [OUTPUT FILE] [CANT DE ITERACIONES PLS] [CANTIDAD ITERACIONES METODO POTENCIA] [CANTIDAD VECINOS]" << endl;
+    cout << "KNN: " << argv[0] << " knn [INPUT FILE TRAIN] [INPUT FILE TEST] [OUTPUT FILE] [CANTIDAD VECINOS]" << endl;
     return 0;
   }
 
   cout << "Parseo del train\n";
   Matriz train = parser(argv[2],-1);
   cout << "Parseo del test\n";
-  Matriz test = parser(argv[3],-1);
+  Matriz test = parserTest2(argv[3],-1);
   float tiempo;
 
   if(strcmp(argv[1],"pca") == 0)
   {
 
     //Copio a cantAutov la cantidad de componentes a considerar (autovectores)
-    int cantAutov = atoi(argv[4]);
+    int cantAutov = atoi(argv[5]);
     //Copio a cantIterMetPot la cantidad de iteraciones para el método de las potencias
-    int cantIterMetPot = atoi(argv[5]);
+    int cantIterMetPot = atoi(argv[6]);
     //Copio a cantVecinos la cantidad de vecinos para kNN
-    int cantVecinos = atoi(argv[6]);
+    int cantVecinos = atoi(argv[7]);
 
     cout << "Comienzo PCA\n";
     //Mido el tiempo
     clock_t t;
     t = clock();
-    usarPca(train, test, cantAutov, cantIterMetPot, cantVecinos);
+    vector<int> res = usarPca(train, test, cantAutov, cantIterMetPot, cantVecinos);
     t = clock() - t;
     tiempo = ((float)t)/CLOCKS_PER_SEC;
+    //Escribimos resultados en archivo
+    ofstream out(argv[4]);
+    cout << "Escribiendo resultados en " << argv[4] << "\n";
+    if (out.is_open())
+    {
+      for (int i = 0; i < res.size(); i++)
+      {
+        out << res[i] << "\n";
+      }
+      out.close();
+    }
+    else cout << "No se pudo abrir " << argv[4] << "\n";
+
   }
   else if (strcmp(argv[1],"pls") == 0)
   {
 
     //Copio a cantAutov la cantidad de componentes a considerar (autovectores)
-    int cantIterPls = atoi(argv[4]);
+    int cantIterPls = atoi(argv[5]);
     //Copio a cantIterMetPot la cantidad de iteraciones para el método de las potencias
-    int cantIterMetPot = atoi(argv[5]);
+    int cantIterMetPot = atoi(argv[6]);
     //Copio a cantVecinos la cantidad de vecinos para kNN
-    int cantVecinos = atoi(argv[6]);
+    int cantVecinos = atoi(argv[7]);
 
     cout << "Comienzo PLS\n";
     //Mido el tiempo
     clock_t t;
     t = clock();
-    usarPls(train, test, cantIterPls, cantIterMetPot, cantVecinos);
+    vector<int> res = usarPls(train, test, cantIterPls, cantIterMetPot, cantVecinos);
     t = clock() - t;
     tiempo = ((float)t)/CLOCKS_PER_SEC;
+    //Escribimos resultados en archivo
+    ofstream out(argv[4]);
+    cout << "Escribiendo resultados en " << argv[4] << "\n";
+    if (out.is_open())
+    {
+      for (int i = 0; i < res.size(); i++)
+      {
+        out << res[i] << "\n";
+      }
+      out.close();
+    }
+    else cout << "No se pudo abrir " << argv[4] << "\n";
   }
   else if (strcmp(argv[1],"knn") == 0)
   {
 
     //Copio a cantVecinos la cantidad de vecinos para kNN
-    int cantVecinos = atoi(argv[4]);
+    int cantVecinos = atoi(argv[5]);
 
     cout << "Comienzo kNN\n";
     //Mido el tiempo
     clock_t t;
     t = clock();
-    usarKnn(train, test, cantVecinos);
+    vector<int> res = usarKnn(train, test, cantVecinos);
     t = clock() - t;
     tiempo = ((float)t)/CLOCKS_PER_SEC;
+    //Escribimos resultados en archivo
+    ofstream out(argv[4]);
+    cout << "Escribiendo resultados en " << argv[4] << "\n";
+    if (out.is_open())
+    {
+      for (int i = 0; i < res.size(); i++)
+      {
+        out << res[i] << "\n";
+      }
+      out.close();
+    }
+    else cout << "No se pudo abrir " << argv[4] << "\n";
   }
 
   cout << "Tiempo empleado (segundos): " << tiempo << "\n";
--- Matriz/parser.cpp
+++ Matriz/parser.cpp
@@ -56,6 +56,53 @@ Matriz parser(char* file, int cantImagenes){ // cantImagenes con -1 es equivalen
 	return m;
 }
 
+Matriz parserTest2(char* file, int cantImagenes){ // cantImagenes con -1 es equivalente a leer todas las imagenes
+
+	Matriz m;
+	
+	ifstream in(file);
+	if (in.is_open()){
+		vector< vector <float> > mtx;
+		vector<int> numeroRepresentado;
+
+		// Parseo de la primer línea
+		string str;
+		getline(in, str);
+		str.clear();
+
+
+		// Parseo del resto del archivo
+		string st;
+		while (getline(in, st) && (cantImagenes != 0)){
+			cantImagenes--;
+			istringstream iss;
+			iss.str(st);
+
+			char aux;
+			iss>> aux;
+			//Aca extraemos la info de la línea en la que estamos
+			vector<float> img;
+			for (int i = 0; i<784; i++){
+
+				int pixel;
+				//Extraemos pixel
+				iss>> pixel;
+				//Extraemos espacio en blanco (solo para avanzar)
+				iss>> aux;
+				img.push_back(pixel);
+			}
+			mtx.push_back(img);
+
+		}
+		Matriz m(mtx,numeroRepresentado);
+		return m;
+	} else {
+		cout << "No se pudo abrir el archivo." << endl;
+	}
+	
+	return m;
+}
+
 
 
 
